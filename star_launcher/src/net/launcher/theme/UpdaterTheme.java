25c50b5e128784ba3cd9569241b9fc8d20acd95d
Administrator <admin@tecman.ru>
2016-08-22 16:39:57 +0300

    cmd

--- star_launcher/src/net/launcher/theme/UpdaterTheme.java
+++ star_launcher/src/net/launcher/theme/UpdaterTheme.java
@@ -11,6 +11,4 @@ public class UpdaterTheme {
    public static int stringsX = 60;
    public static int stringsY = 370;
    public static FontBundle updaterDesc = new FontBundle("font", 18.0F, Color.decode("0xA67A53"));
-
-
 }
